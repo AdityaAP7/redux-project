d04135b0707fc631e0b5f3187aa47d8dbab6e202
AdityaAP7 <Adityapalodkar@gmail.com>
2024-05-26 15:18:51 +0530

    Removed the test code

--- src/App.js
+++ src/App.js
@@ -3,17 +3,11 @@ import "./App.css";
 import User from "./components/User";
 
 function App() {
-  const user = ["apple", "kivi", "banana", "watermelon"];
 
-  const filterUser = user.filter((item) => 
-    item[0] === "a"
-  );
 
   return (
     <div className="App">
-      {filterUser.map((item, index) => {
-        return <User name={item} key={index} />;
-      })}
+   
     </div>
   );
 }
