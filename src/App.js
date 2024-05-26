fe9f18180a7866859adb12f02e250537591b1b50
AdityaAP7 <Adityapalodkar@gmail.com>
2024-05-26 17:05:55 +0530

    Creatd Cards UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import User from "./components/User";
+import Home from "./components/Home";
 
 function App() {
 
 
   return (
     <div className="App">
-   
+   <Home/>
     </div>
   );
 }
