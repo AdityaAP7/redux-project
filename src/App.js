d3307244e07660ab6bfb5259c1fa7277efe3f00a
AdityaAP7 <Adityapalodkar@gmail.com>
2024-05-26 15:01:25 +0530

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import User from "./components/User";
 
 function App() {
+  const user = ["apple", "kivi", "banana", "watermelon"];
+
+  const filterUser = user.filter((item) => 
+    item[0] === "a"
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {filterUser.map((item, index) => {
+        return <User name={item} key={index} />;
+      })}
     </div>
   );
 }
