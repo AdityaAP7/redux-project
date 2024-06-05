b2a5583aacd361274717d71f3b56f77c78d23978
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-06-05 23:50:05 +0530

    Update actions.js

--- src/services/Actions/actions.js
+++ src/services/Actions/actions.js
@@ -13,6 +13,18 @@ export const removeFromCart =(data)=>{
         type:"ADD_TO_CART",
         data:data
     }
+}export  const addToCart =(data)=>{
+    return {
+        type:"ADD_TO_CART",
+        data:data
+    }
+}
+
+export const removeFromCart =(data)=>{
+    return {
+        type:"REMOVE_FROM_CART",
+        data:data
+    }
 }
 
 export const removeFromCart =(data)=>{
