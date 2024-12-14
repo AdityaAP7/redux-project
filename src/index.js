ecabd33d26ee00f2fe3fe8aaecd486cbed601500
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-12-14 23:56:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-111111111111111111
+1111111111111111111
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
