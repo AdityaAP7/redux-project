daac36f7c7aaad7d0f8c51e3525978bb8e52648b
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-13 23:47:42 +0530

    Update reducer.js

--- src/services/Reducers/reducer.js
+++ src/services/Reducers/reducer.js
@@ -52,7 +52,10 @@ const addToCart = (item)
       };return state;
   }const removeFromCart = (itemId) => ({
   type: REMOVE_FROM_CART,
-  payload: itemId
+  payload: itemIdcartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
         ...state,
         cartItems: state.cartItems.filter(item => item.id !== action.payload)
       };  case REMOVE_FROM_CART:
